ad758001e55efd90f7dc26e1d9e8d0e03fb444ab
LDSchaefer <45575050+LDSchaefer@users.noreply.github.com>
2019-11-12 23:06:33 +0100

    Add files via upload

--- milestones_1_c/main.cpp
+++ milestones_1_c/main.cpp
@@ -54,28 +54,7 @@ public:
                    tem_arr[i * size + j] = r;
                }
            }
-        tem_arr[0 * size + 0] = 1;
-        tem_arr[1 * size + 0] = 1;
-        tem_arr[2 * size + 0] = 1;
-        tem_arr[0 * size + 2] = 1;
-        tem_arr[0 * size + 3] = 1;
-        tem_arr[1 * size + 3] = 1;
-        tem_arr[2 * size + 3] = 1;
-        tem_arr[2 * size + 2] = 1;
-        tem_arr[3 * size + 3] = 1;
-        tem_arr[3 * size + 2] = 1;
-        tem_arr[3 * size + 4] = 1;
-        tem_arr[9 * size + 9] = 1;
         return tem_arr;
-    }
-
-
-    int *copy_array()
-    {
-        int *temp_arr = arr;
-        arr = old_arr;
-        old_arr = temp_arr;
-        return arr;
     }
 
 
@@ -107,65 +86,60 @@ public:
                 {
                     for(int l=j-1; l<=j+1; l++)
                     {
-                         if(k==l)
-                         {
-                         continue;
-                         }
-                         else
-                         {
-                             k_temp = k;
-                             l_temp = l;
-                             if(k<0)
-                             {
-                                 k_temp = k + a;
-                             }
-                             if(l<0)
-                             {
-                                 l_temp = l + b;
-
-                             }
-                             if(k>a)
-                             {
-                                 k_temp = k - a;
-                             }
-                             if(l>b)
-                             {
-                                 l_temp = l - b;
-
-                             }
-                             if(arr[k_temp * size + l_temp] == 1)
-                             {
-                             count++;
-                             }
-                         }
+                        if(k==i and l==j)
+                        {
+                        continue;
+                        }
+                        else
+                        {
+                            k_temp = k;
+                            l_temp = l;
+                            if(k<0)
+                            {
+                                k_temp = k + a;
+                            }
+                            if(l<0)
+                            {
+                                l_temp = l + b;
+                            }
+                            if((k+1)>a)
+                            {
+                                k_temp = k - a;
+                            }
+                            if((l+1)>b)
+                            {
+                                l_temp = l - b;
+                            }
+                            if(arr[k_temp * size + l_temp] == 1)
+                            {
+                                count++;
+                            }
+                        }
                     }
                 }
-
                 if(arr[i * size + j] == 1 and count<=1)
                 {
                     old_arr[i * size + j] = 0;
-                    std::cout<<count;
                 }
                 if(arr[i * size + j] == 1 and (count== 2 or count==3))
                 {
                     old_arr[i * size + j] = 1;
-                    std::cout<<count;
                 }
                 if(arr[i * size + j] == 1 and count>=4)
                 {
                     old_arr[i * size + j] = 0;
-                    std::cout<<count;
                 }
                 if(arr[i * size + j] == 0 and count==3)
                 {
                     old_arr[i * size + j] = 1;
-                    std::cout<<count;
                 }
                 count = 0;
+
             }
         }
         return arr;
     }
+
 
     int to_int(std::string str)
     {
@@ -174,7 +148,7 @@ public:
         val>>x;
         return x;
     }
-    int import(std::string i_path){
+    int *import(std::string i_path){
         std::ifstream data(i_path + ".txt");
         std::string colum;
         std::getline(data, colum);
@@ -200,7 +174,7 @@ public:
             }
         }
         print(a,b,arr);
-        return 0;
+        return old_arr = arr;
     }
 
 
@@ -218,11 +192,11 @@ public:
                     char c;
                     if(arr[i * size + j] == 0)
                     {
-                        c=arr[i * size + j]+111;
+                        c = (arr[i * size + j]+111);
                     }
                     else
                     {
-                        c= arr[i * size + j]+41;
+                        c = (arr[i * size + j]+41);
                     }
                     outp<< c;
                     if((j+1)%a == 0)
@@ -313,11 +287,5 @@ public:
 int main()
 {
     Field f(10,10);
-
-//    std::ofstream outp;
-//    outp.open("output.txt");
-//    outp<< f.a;
-//    outp<< f.b;
-
     return 0;
 }
